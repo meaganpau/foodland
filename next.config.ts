60ad73c0eb47444bf8c3c4d20f7632e889c5702e
Meagan Pau <meaganpau@users.noreply.github.com>
2025-08-04 14:29:58 -0400

    disable experimental

--- next.config.ts
+++ next.config.ts
@@ -11,9 +11,6 @@ const nextConfig: NextConfig = {
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
     minimumCacheTTL: 60,
   },
-  experimental: {
-    optimizeCss: true,
-  },
   async headers() {
     return [
       {
